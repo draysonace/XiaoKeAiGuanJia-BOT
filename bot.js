73c6e05d1cd8360dcdbdd735f3e92708fd8d971b
Drayson <39640027+draysonace@users.noreply.github.com>
2018-05-27 15:41:31 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,19 +8,18 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'r6 dray') {
     	message.reply({embed: {
-  "description": "[Dray7z | R6DB](https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657)",
+  "description": "[Dray7z](https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657)",
     "url": "https://discordapp.com",
     "color": 16777215,
-    "timestamp": "2018-05-27T04:27:54.100Z",
     "footer": {
       "icon_url": "https://ubistatic19-a.akamaihd.net/resource/it-it/game/rainbow6/siege-v3/r6-operators-list-jager_317272.png",
-      "text": "Jäger"
+      "text": "Jäger | Developed by Drayson"
     },
     "thumbnail": {
       "url": "https://uplay-avatars.s3.amazonaws.com/a633df64-18bf-4f2d-a314-d75ab9c77657/default_146_146.png"
     },
     "author": {
-      "name": (client.user),
+      "name": "R6DB",
       "url": "https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657",
       "icon_url": "https://i.redd.it/iznunq2m8vgy.png"
     }
