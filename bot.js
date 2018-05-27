7f945876d0be4c0eac20aabcdba16dc10f3b475f
Drayson <39640027+draysonace@users.noreply.github.com>
2018-05-27 12:01:28 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,13 @@ client.on('message', message => {
   description: "[Dray7z | R6DB](https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657)"
 }});
   	}
+	
+	if else (message.content === 'r6 jager') {
+    	message.reply({embed: {
+  color: 3447003,
+  description: "[Fundy123 | R6DB](https://r6db.com/player/0ce52d73-e597-4aad-b2ac-3733b9c37194)"
+}});
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
