d631f43017ffeb1c11d329ae8162d90790c1bd48
Drayson <39640027+draysonace@users.noreply.github.com>
2018-05-27 11:40:47 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'r6 dray') {
-    	message.reply('embed=discord.Embed(title="Dray7z | R6DB", url="https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657")
-embed.add_field(name=, value=undefined, inline=False)
-await self.bot.say(embed=embed)');
+    	message.reply('embed= {
+                      [Dray7z | R6DB](https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657)
+                                      }');
   	}
 });
 
