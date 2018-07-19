c9dec967c0a275d8f2db0b96198969cc0f2b6c08
Drayson <39640027+draysonace@users.noreply.github.com>
2018-07-19 13:08:44 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,42 +6,36 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'r6 dray') {
-    	message.reply({embed: {
-  "description": "[Dray7z](https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657)",
-    "color": 16777215,
-    "footer": {
-      "icon_url": "https://ubistatic19-a.akamaihd.net/resource/it-it/game/rainbow6/siege-v3/r6-operators-list-jager_317272.png",
-      "text": "Jäger"
-    },
-    "thumbnail": {
-      "url": "https://uplay-avatars.s3.amazonaws.com/a633df64-18bf-4f2d-a314-d75ab9c77657/default_146_146.png"
-    },
-    "author": {
-      "name": "R6DB",
-      "url": "https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657",
-      "icon_url": "https://i.redd.it/iznunq2m8vgy.png"
-    }
-}});
+    if (message.content === '早安') {
+    	message.reply('早安');
   	}
 	
-	else if(message.content === 'r6 jager') {
-    	message.reply({embed: {
-  "description": "[Fundy123](https://r6db.com/player/0ce52d73-e597-4aad-b2ac-3733b9c37194)",
-    "color": 16777215,
-    "footer": {
-      "icon_url": "https://ubistatic19-a.akamaihd.net/resource/it-it/game/rainbow6/siege-v3/r6-operators-list-jager_317272.png",
-      "text": "Jäger"
-    },
-    "thumbnail": {
-      "url": "https://uplay-avatars.s3.amazonaws.com/0ce52d73-e597-4aad-b2ac-3733b9c37194/default_146_146.png"
-    },
-    "author": {
-      "name": "R6DB",
-      "url": "https://r6db.com/player/a633df64-18bf-4f2d-a314-d75ab9c77657",
-      "icon_url": "https://i.redd.it/iznunq2m8vgy.png"
-	  }
-}});
+	else if(message.content === '午安') {
+    	message.reply('午安');
+  	}
+	
+	else if(message.content === '晚安') {
+    	message.reply('晚安');
+  	}
+	
+	else if(message.content === '安安') {
+    	message.reply('安安');
+  	}
+	
+	else if(message.content === '大家早安') {
+    	message.reply('早安');
+  	}
+	
+	else if(message.content === '大家午安') {
+    	message.reply('午安');
+  	}
+	
+	else if(message.content === '大家晚安') {
+    	message.reply('晚安');
+  	}
+	
+	else if(message.content === '大家安安') {
+    	message.reply('安安');
   	}
 });
 
